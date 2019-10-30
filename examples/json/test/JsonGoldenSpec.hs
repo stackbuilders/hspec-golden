a7fd6a1817b153c55069126eb5fb3eaf64dedb33
Jorge Guerra <jguerra@stackbuilders.com>
2019-10-30 13:12:05 -0500

    Add a JSON Example

---
+++ examples/json/test/JsonGoldenSpec.hs
@@ -0,0 +1,26 @@
+module JsonGoldenSpec where
+
+import           Test.Hspec
+import           Test.Hspec.Golden
+import           Json
+import qualified Data.ByteString.Lazy as B         
+
+
+goldenBytestring :: String -> B.ByteString -> Golden B.ByteString
+goldenBytestring name actualOutput =
+    Golden {
+        output = actualOutput,
+        encodePretty = show,
+        writeToFile = B.writeFile,
+        readFromFile = B.readFile,
+        testName = name,
+        directory = ".otherGolden"
+          
+    }
+
+
+spec :: Spec
+spec =
+  describe "encodeCountries" $ do
+   it "encodes a group of Countries into a JSON bytestring " $
+    goldenBytestring "json" (encodeCountries countries)
