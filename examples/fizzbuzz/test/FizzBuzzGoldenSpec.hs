1111512fd37bad021b86c693bae62680f207cf4a
Jorge Guerra <jguerra@stackbuilders.com>
2019-10-30 13:24:21 -0500

    Add a FizzBuzz example

---
+++ examples/fizzbuzz/test/FizzBuzzGoldenSpec.hs
@@ -0,0 +1,11 @@
+module FizzBuzzGoldenSpec where
+
+import           Test.Hspec
+import           Test.Hspec.Golden
+import           FizzBuzz
+
+spec :: Spec
+spec = 
+    describe "fizzBuzz" $
+      it "Turns 3 multiples to fizz and 5 multiples to buzz" $
+        defaultGolden "fizzbuzz" (show $ fizzBuzz [1,2,3,4,5,11,12,13,14,15])
