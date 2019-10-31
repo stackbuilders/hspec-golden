f361111d5b5feeeda497c8eaf32dd33a00f3c823
Jorge Guerra Landázuri <jagl257@gmail.com>
2019-10-31 16:10:00 -0500

    Jguerra hspec golden examples (#11)
    * Add a JSON Example
    
    * Add a FizzBuzz example
    
    * Updated
    
    * Minor Changes
    
    * Update Readme
    
    * Update Readme

---
+++ examples/fizzbuzz/README.md
@@ -0,0 +1,6 @@
+# FizzBuzz 
+
+This is a little example about how we can use ```hspec-golden``` to test our outputs.
+
+Try changing the fizzbuzz parameter and update tests with the hgold CLI.
+
---
+++ examples/json/src/Json.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module Json where
+
+import            Data.Aeson                                  (ToJSON, encode)
+import            GHC.Generics                                (Generic)
+import            Data.ByteString.Lazy                        (ByteString)
+
+
+
+data Country = Country
+  {
+    cname      :: String,
+    continent :: String,
+    ctag       :: Int
+  } deriving (Generic, Show)
+
+instance ToJSON Country
+
+
+ecuador = Country "Ecuador" "America" 1
+germany = Country "Germany" "Europe" 2
+japan = Country "Japan" "Asia" 4
+
+countries :: [Country]
+countries = [ecuador,germany,japan]
+
+encodeCountries :: [Country] -> ByteString
+encodeCountries = encode
