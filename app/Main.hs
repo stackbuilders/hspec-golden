1ec19af5b9024601f7dc9b1c40e376b17482a5d1
CristhianMotoche <cristhian.motoche@gmail.com>
2024-07-24 08:30:56 -0500

    feat(release): Update print testing x5

--- app/Main.hs
+++ app/Main.hs
@@ -48,7 +48,7 @@ failure = withColor Red
 -- Update golden files in the given directory
 updateGolden :: FilePath -> IO ()
 updateGolden dir = do
-  putStrLn "TESTING!!! x4"
+  putStrLn "TESTING!!! x5"
   putStrLn "Replacing golden with actual:"
   go dir
   success $ putStrLn "Finished!"
